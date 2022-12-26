7088a6d2af5f1434831302359673fad66f0d7b70
d2nst <wlsdl950@gmail.com>
2022-12-26 22:19:36 +0900

    메뉴 활성화

--- js/main.js
+++ js/main.js
@@ -1,11 +1,20 @@
-const burger = document.querySelectorAll('.burger-bar');
+const burger = document.querySelector('.burger-bar');
 const showMenu = document.querySelector('.overlay');
 const shadowBg = document.querySelector('.overlay-wrap');
 
-burger.forEach((icon) => {
-  icon.addEventListener('click', (e) => {
-    icon.classList.toggle('open');
-    showMenu.classList.toggle('visible');
-    shadowBg.classList.toggle('shadow');
+burger.addEventListener('click', () => {
+  burger.classList.toggle('open');
+  showMenu.classList.toggle('visible');
+  shadowBg.classList.toggle('shadow');
+});
+
+//
+
+const menuActive = document.querySelectorAll('.site-menu li a');
+
+menuActive.forEach((element) => {
+  element.addEventListener('click', function () {
+    menuActive.forEach((nav) => nav.classList.remove('active'));
+    this.classList.add('active');
   });
 });
