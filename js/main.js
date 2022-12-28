15b56e17978400882bfe5c9969b036a7b5303813
d2nst <wlsdl950@gmail.com>
2022-12-29 00:25:35 +0900

    section 3번째 완성

--- js/main.js
+++ js/main.js
@@ -1,5 +1,5 @@
 // menu 활성화
-const menuActive = document.querySelectorAll('#overlay ul li a');
+const menuActive = document.querySelectorAll('.overlay ul li a');
 
 menuActive.forEach((element) => {
   element.addEventListener('click', function () {
@@ -10,9 +10,29 @@ menuActive.forEach((element) => {
 
 // 햄버거바 클릭 시, 엑스표시 변함 & 메뉴바 열림
 const burger = document.querySelector('.burger-bar');
-const overlay = document.querySelector('#overlay');
+const overlay = document.querySelector('.overlay');
 
 burger.addEventListener('click', () => {
   burger.classList.toggle('open');
   overlay.classList.toggle('visible');
 });
+
+overlay.addEventListener('click', () => {
+  burger.classList.toggle('open');
+  overlay.classList.toggle('visible');
+});
+
+// gsap
+gsap.registerPlugin(ScrollTrigger);
+
+gsap.to('.design__left', {
+  yPercent: -100,
+  ease: 'none',
+  scrollTrigger: {
+    trigger: '.main__design',
+    scrub: true,
+  },
+});
+
+// AOS animation
+AOS.init();
